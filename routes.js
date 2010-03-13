8132495f90d6ea5a50792836557e3c0c568a3c2e
defrex <defrex@laps.(none)>
2010-03-13 09:46:08 -0500

    fixed for newer api

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 
 var sys = require('sys');
+var parseURL = require('url').parse;
 
-var route = function(req, res, urls){
+var route = function(req, res, urls, passed_args){
     for (var i=0;i<urls.length;i++){
-        var args = new RegExp(urls[i][0]).exec(req.uri.path);
+        var args = new RegExp(urls[i][0]).exec(parseUrl(req.url).pathname);
         if (args !== null){
             args.shift();
             args.unshift(req, res);
+            if (typeof passed_args == 'array')
+                args.concat(passed_args);
             urls[i][1].apply(this, args);
             break;
         }
@@ -14,7 +17,9 @@ var route = function(req, res, urls){
 };
 
 var include = function(urls){
-    return function(req, res){route(req, res, urls);};
+    return function(req, res){
+        route(req, res, urls, Array.prototype.slice.call(arguments, 2));
+    };
 };
 
 exports.route = route;
